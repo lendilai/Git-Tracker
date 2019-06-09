7a04d6249b07e451110e74de6b284d765163fb87
lendilai <ngethenan768@gmail.com>
2019-06-09 09:51:31 +0300

    Adds a promise to the component

--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -16,7 +16,35 @@ export class SearchComponent implements OnInit {
 
   constructor(private userData: SearchUserService) { }
 
+  public getUser(event: any){
+    let promise = new Promise((resolve, reject)=>{
+      this.userData.getUser(this.userName).toPromise().then(response => {
+        this.users = response;
 
+        resolve();
+      },
+      error => {
+        this.users =  "An unexpected error has occurred";
+      }
+    );
+  });
+  return promise;
+  }
+
+  public getRepos(event: any){
+    let promise = new Promise((resolve, reject)=>{
+      this.userData.getRepos(this.userName).toPromise().then(response => {
+        this.repos = response;
+
+        resolve();
+      },
+      error => {
+        this.repos =  "An unexpected error has occurred";
+      }
+    );
+  });
+  return promise;
+  }
   ngOnInit() {
   }
 
