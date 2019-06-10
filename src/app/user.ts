f4f468287827b40d78e1ea7b88a48a85a148d396
lendilai <ngethenan768@gmail.com>
2019-06-10 11:07:29 +0300

    Styles the search page better

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,11 +1,11 @@
 export class User {
   name: string;
-  login: number;
+  login: string;
   avatar_url: any;
   followers_url: number;
   following_url: number;
   public_repos: string;
-  created_at: string;
+  created_at: date;
   html_url: string;
   bio: string;
   location: string;
